088de6778a17c0c02e8a586801e24686a8e85a70
Leskov Andrew <102716018+und3rrr@users.noreply.github.com>
2024-11-26 18:06:35 +0300

    лабораторная работа 3

---
+++ ImageTransformation/ImageTransformation/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ImageTransformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadImage = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTransformed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBoxResult2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTransformed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoadImage
+            // 
+            this.buttonLoadImage.Location = new System.Drawing.Point(514, 92);
+            this.buttonLoadImage.Name = "buttonLoadImage";
+            this.buttonLoadImage.Size = new System.Drawing.Size(122, 23);
+            this.buttonLoadImage.TabIndex = 0;
+            this.buttonLoadImage.Text = "LoadImage";
+            this.buttonLoadImage.UseVisualStyleBackColor = true;
+            this.buttonLoadImage.Click += new System.EventHandler(this.buttonLoadImage_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AffineTransform";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonAffineTransform_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "FunctionalTransform";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonFunctionalTransform_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(514, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SaveResult";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonSaveResult_Click);
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(209, 140);
+            this.pictureBoxOriginal.TabIndex = 5;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxTransformed
+            // 
+            this.pictureBoxTransformed.Location = new System.Drawing.Point(264, 12);
+            this.pictureBoxTransformed.Name = "pictureBoxTransformed";
+            this.pictureBoxTransformed.Size = new System.Drawing.Size(209, 140);
+            this.pictureBoxTransformed.TabIndex = 6;
+            this.pictureBoxTransformed.TabStop = false;
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.Location = new System.Drawing.Point(13, 210);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(208, 140);
+            this.pictureBoxResult.TabIndex = 7;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(514, 210);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "InverseAffineTransform";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttonInverseAffineTransform_Click);
+            // 
+            // pictureBoxResult2
+            // 
+            this.pictureBoxResult2.Location = new System.Drawing.Point(264, 210);
+            this.pictureBoxResult2.Name = "pictureBoxResult2";
+            this.pictureBoxResult2.Size = new System.Drawing.Size(209, 140);
+            this.pictureBoxResult2.TabIndex = 9;
+            this.pictureBoxResult2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 362);
+            this.Controls.Add(this.pictureBoxResult2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Controls.Add(this.pictureBoxTransformed);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonLoadImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTransformed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadImage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxTransformed;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBoxResult2;
+    }
+}
+
---
+++ ImageTransformation/ImageTransformation/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageTransformation
+{
+    public partial class Form1 : Form
+    {
+        Bitmap originalImage;
+        Bitmap transformedImage;
+        double shearX = 0.2;  // Значение скоса
+        double scaleY = 1.2;  // Значение масштабирования
+        double coshFactor = 0.01; // Фактор для нелинейного преобразования
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Загрузка изображения
+        private void buttonLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog.FileName);
+                pictureBoxOriginal.Image = originalImage; // Отображение исходного изображения
+                pictureBoxOriginal.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        // Асинхронные аффинные преобразования
+        private async void buttonAffineTransform_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Пожалуйста, загрузите изображение.");
+                return;
+            }
+
+            transformedImage = await Task.Run(() => AffineTransform(originalImage));
+            pictureBoxTransformed.Image = transformedImage;
+            pictureBoxTransformed.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private async void buttonInverseAffineTransform_Click(object sender, EventArgs e)
+        {
+            if (transformedImage == null)
+            {
+                MessageBox.Show("Сначала выполните аффинные преобразования.");
+                return;
+            }
+
+            Bitmap resultImage = await Task.Run(() => InverseAffineTransform(transformedImage));
+            pictureBoxResult2.Image = resultImage;
+            pictureBoxResult2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        // Асинхронное функциональное преобразование
+        private async void buttonFunctionalTransform_Click(object sender, EventArgs e)
+        {
+            if (transformedImage == null)
+            {
+                MessageBox.Show("Сначала выполните аффинные преобразования.");
+                return;
+            }
+
+            Bitmap resultImage = await Task.Run(() => FunctionalTransform(transformedImage));
+            pictureBoxResult.Image = resultImage;
+            pictureBoxResult.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        // Сохранение результата в файл
+        private void buttonSaveResult_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxResult.Image == null)
+            {
+                MessageBox.Show("Сначала выполните преобразования изображения.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxResult.Image.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                MessageBox.Show("Изображение сохранено.");
+            }
+        }
+
+        // Метод для прямого аффинного преобразования (скос и масштабирование)
+        private Bitmap AffineTransform(Bitmap image)
+        {
+            Bitmap newImage = new Bitmap(image.Width, image.Height);
+            var rect = new Rectangle(0, 0, image.Width, image.Height);
+            var imageData = image.LockBits(rect, ImageLockMode.ReadOnly, image.PixelFormat);
+            var newImageData = newImage.LockBits(rect, ImageLockMode.WriteOnly, newImage.PixelFormat);
+
+            unsafe
+            {
+                byte* sourcePtr = (byte*)imageData.Scan0;
+                byte* destPtr = (byte*)newImageData.Scan0;
+                int bpp = Image.GetPixelFormatSize(image.PixelFormat) / 8;
+
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        int newX = (int)(x + shearX * y);
+                        int newY = (int)(y * scaleY);
+
+                        if (newX >= 0 && newX < image.Width && newY >= 0 && newY < image.Height)
+                        {
+                            byte* pixel = sourcePtr + (y * imageData.Stride + x * bpp);
+                            byte* newPixel = destPtr + (newY * newImageData.Stride + newX * bpp);
+
+                            for (int i = 0; i < bpp; i++)
+                                newPixel[i] = pixel[i];
+                        }
+                    }
+                }
+            }
+
+            image.UnlockBits(imageData);
+            newImage.UnlockBits(newImageData);
+            return newImage;
+        }
+
+        // Обратное аффинное преобразование
+        private Bitmap InverseAffineTransform(Bitmap image)
+        {
+            Bitmap newImage = new Bitmap(image.Width, image.Height);
+            var rect = new Rectangle(0, 0, image.Width, image.Height);
+            var imageData = image.LockBits(rect, ImageLockMode.ReadOnly, image.PixelFormat);
+            var newImageData = newImage.LockBits(rect, ImageLockMode.WriteOnly, newImage.PixelFormat);
+
+            unsafe
+            {
+                byte* sourcePtr = (byte*)imageData.Scan0;
+                byte* destPtr = (byte*)newImageData.Scan0;
+                int bpp = Image.GetPixelFormatSize(image.PixelFormat) / 8;
+
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        int originalX = (int)(x - shearX * y);
+                        int originalY = (int)(y / scaleY);
+
+                        if (originalX >= 0 && originalX < image.Width && originalY >= 0 && originalY < image.Height)
+                        {
+                            byte* pixel = sourcePtr + (originalY * imageData.Stride + originalX * bpp);
+                            byte* newPixel = destPtr + (y * newImageData.Stride + x * bpp);
+
+                            for (int i = 0; i < bpp; i++)
+                                newPixel[i] = pixel[i];
+                        }
+                    }
+                }
+            }
+
+            image.UnlockBits(imageData);
+            newImage.UnlockBits(newImageData);
+            return newImage;
+        }
+
+        // Функциональное преобразование i = cosh(x') - 1, j = y'
+        private Bitmap FunctionalTransform(Bitmap image)
+        {
+            Bitmap newImage = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    int newX = (int)(Math.Cosh(x * coshFactor) - 1); // Нелинейное преобразование X
+                    int newY = y;  // Y остается неизменным
+
+                    if (newX >= 0 && newX < image.Width && newY >= 0 && newY < image.Height)
+                    {
+                        newImage.SetPixel(newX, newY, image.GetPixel(x, y));
+                    }
+                }
+            }
+
+            return newImage;
+        }
+    }
+}
