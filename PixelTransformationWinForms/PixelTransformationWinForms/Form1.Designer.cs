fa2ee1acc920574b376116273de38b46210eabd0
Leskov Andrew <102716018+und3rrr@users.noreply.github.com>
2024-11-26 18:53:48 +0300

    лабораторая 4

---
+++ PixelTransformationWinForms/PixelTransformationWinForms/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PixelTransformationWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxBase = new System.Windows.Forms.PictureBox();
+            this.buttonLoadBaseImage = new System.Windows.Forms.Button();
+            this.buttonLoadOverlayImage = new System.Windows.Forms.Button();
+            this.buttonProcessImages = new System.Windows.Forms.Button();
+            this.pictureBoxOverlay = new System.Windows.Forms.PictureBox();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.buttonSaveResult = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOverlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxBase
+            // 
+            this.pictureBoxBase.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxBase.Name = "pictureBoxBase";
+            this.pictureBoxBase.Size = new System.Drawing.Size(351, 252);
+            this.pictureBoxBase.TabIndex = 0;
+            this.pictureBoxBase.TabStop = false;
+            // 
+            // buttonLoadBaseImage
+            // 
+            this.buttonLoadBaseImage.Location = new System.Drawing.Point(406, 32);
+            this.buttonLoadBaseImage.Name = "buttonLoadBaseImage";
+            this.buttonLoadBaseImage.Size = new System.Drawing.Size(108, 23);
+            this.buttonLoadBaseImage.TabIndex = 2;
+            this.buttonLoadBaseImage.Text = "LoadBaseImage";
+            this.buttonLoadBaseImage.UseVisualStyleBackColor = true;
+            this.buttonLoadBaseImage.Click += new System.EventHandler(this.buttonLoadBaseImage_Click);
+            // 
+            // buttonLoadOverlayImage
+            // 
+            this.buttonLoadOverlayImage.Location = new System.Drawing.Point(406, 96);
+            this.buttonLoadOverlayImage.Name = "buttonLoadOverlayImage";
+            this.buttonLoadOverlayImage.Size = new System.Drawing.Size(108, 23);
+            this.buttonLoadOverlayImage.TabIndex = 3;
+            this.buttonLoadOverlayImage.Text = "LoadOverlayImage";
+            this.buttonLoadOverlayImage.UseVisualStyleBackColor = true;
+            this.buttonLoadOverlayImage.Click += new System.EventHandler(this.buttonLoadOverlayImage_Click);
+            // 
+            // buttonProcessImages
+            // 
+            this.buttonProcessImages.Location = new System.Drawing.Point(406, 158);
+            this.buttonProcessImages.Name = "buttonProcessImages";
+            this.buttonProcessImages.Size = new System.Drawing.Size(108, 23);
+            this.buttonProcessImages.TabIndex = 4;
+            this.buttonProcessImages.Text = "ProcessImages";
+            this.buttonProcessImages.UseVisualStyleBackColor = true;
+            this.buttonProcessImages.Click += new System.EventHandler(this.buttonProcessImages_Click);
+            // 
+            // pictureBoxOverlay
+            // 
+            this.pictureBoxOverlay.Location = new System.Drawing.Point(559, 13);
+            this.pictureBoxOverlay.Name = "pictureBoxOverlay";
+            this.pictureBoxOverlay.Size = new System.Drawing.Size(339, 251);
+            this.pictureBoxOverlay.TabIndex = 5;
+            this.pictureBoxOverlay.TabStop = false;
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.Location = new System.Drawing.Point(279, 284);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(351, 251);
+            this.pictureBoxResult.TabIndex = 6;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // buttonSaveResult
+            // 
+            this.buttonSaveResult.Location = new System.Drawing.Point(420, 209);
+            this.buttonSaveResult.Name = "buttonSaveResult";
+            this.buttonSaveResult.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveResult.TabIndex = 7;
+            this.buttonSaveResult.Text = "SaveResult";
+            this.buttonSaveResult.UseVisualStyleBackColor = true;
+            this.buttonSaveResult.Click += new System.EventHandler(this.buttonSaveResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 556);
+            this.Controls.Add(this.buttonSaveResult);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Controls.Add(this.pictureBoxOverlay);
+            this.Controls.Add(this.buttonProcessImages);
+            this.Controls.Add(this.buttonLoadOverlayImage);
+            this.Controls.Add(this.buttonLoadBaseImage);
+            this.Controls.Add(this.pictureBoxBase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOverlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxBase;
+        private System.Windows.Forms.Button buttonLoadBaseImage;
+        private System.Windows.Forms.Button buttonLoadOverlayImage;
+        private System.Windows.Forms.Button buttonProcessImages;
+        private System.Windows.Forms.PictureBox pictureBoxOverlay;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.Button buttonSaveResult;
+    }
+}
+
---
+++ PixelTransformationWinForms/PixelTransformationWinForms/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace PixelTransformationWinForms
+{
+    public partial class Form1 : Form
+    {
+        Bitmap baseImage;
+        Bitmap overlayImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Загрузка основного изображения
+        // Загрузка основного изображения
+        private void buttonLoadBaseImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                baseImage = new Bitmap(openFileDialog.FileName);
+                pictureBoxBase.Image = baseImage; // отображение изображения
+                pictureBoxBase.SizeMode = PictureBoxSizeMode.Zoom; // Установка масштаба
+            }
+        }
+
+        // Загрузка накладываемого изображения
+        private void buttonLoadOverlayImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                overlayImage = new Bitmap(openFileDialog.FileName);
+                pictureBoxOverlay.Image = overlayImage; // отображение изображения
+                pictureBoxOverlay.SizeMode = PictureBoxSizeMode.Zoom; // Установка масштаба
+            }
+        }
+
+        // Обработка изображения
+        // Обработка изображения
+        private void buttonProcessImages_Click(object sender, EventArgs e)
+        {
+            if (baseImage == null || overlayImage == null)
+            {
+                MessageBox.Show("Пожалуйста, загрузите оба изображения.");
+                return;
+            }
+
+            // Преобразование основного изображения с синусом тона
+            Bitmap transformedImage = TransformImageWithSineTone(baseImage);
+
+            // Наложение второго изображения
+            Bitmap resultImage = ApplyOverlay(transformedImage, overlayImage);
+
+            // Показ результата
+            pictureBoxResult.Image = resultImage;
+            pictureBoxResult.SizeMode = PictureBoxSizeMode.Zoom; // Установка масштаба
+        }
+
+
+        // Метод преобразования изображения с синусом от тона
+        private Bitmap TransformImageWithSineTone(Bitmap image)
+        {
+            Bitmap newImage = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+
+                    // Преобразование RGB в HSV
+                    float hue, saturation, value;
+                    RgbToHsv(pixel, out hue, out saturation, out value);
+
+                    // Применение синуса от тона к насыщенности
+                    saturation = (float)Math.Abs(Math.Sin(hue * Math.PI / 180));
+
+                    // Преобразование обратно в RGB
+                    Color newPixel = HsvToRgb(hue, saturation, value);
+                    newImage.SetPixel(x, y, newPixel);
+                }
+            }
+            return newImage;
+        }
+
+        // Метод наложения одного изображения на другое
+        private Bitmap ApplyOverlay(Bitmap baseImage, Bitmap overlayImage)
+        {
+            Bitmap resultImage = new Bitmap(baseImage.Width, baseImage.Height);
+
+            for (int y = 0; y < baseImage.Height; y++)
+            {
+                for (int x = 0; x < baseImage.Width; x++)
+                {
+                    Color basePixel = baseImage.GetPixel(x, y);
+                    Color overlayPixel = overlayImage.GetPixel(x % overlayImage.Width, y % overlayImage.Height); // повторение наложения
+
+                    // Пример наложения - среднее арифметическое (можно изменить правило 13)
+                    int r = (basePixel.R + overlayPixel.R) / 2;
+                    int g = (basePixel.G + overlayPixel.G) / 2;
+                    int b = (basePixel.B + overlayPixel.B) / 2;
+
+                    resultImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            return resultImage;
+        }
+
+        // Сохранение результата в файл
+        private void buttonSaveResult_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxResult.Image == null)
+            {
+                MessageBox.Show("Сначала выполните обработку изображений.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxResult.Image.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                MessageBox.Show("Изображение сохранено.");
+            }
+        }
+
+        // Метод преобразования RGB в HSV
+        public static void RgbToHsv(Color color, out float hue, out float saturation, out float value)
+        {
+            float r = color.R / 255f;
+            float g = color.G / 255f;
+            float b = color.B / 255f;
+
+            float max = Math.Max(r, Math.Max(g, b));
+            float min = Math.Min(r, Math.Min(g, b));
+
+            hue = color.GetHue();
+            saturation = (max == 0) ? 0 : (1f - (1f * min / max));
+            value = max;
+        }
+
+        // Метод преобразования HSV в RGB
+        public static Color HsvToRgb(float hue, float saturation, float value)
+        {
+            // Убедимся, что hue находится в диапазоне [0, 360)
+            if (hue < 0) hue += 360;
+            if (hue >= 360) hue -= 360;
+
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            float f = hue / 60 - (int)(hue / 60); // Явное преобразование в int
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            switch (hi)
+            {
+                case 0:
+                    return Color.FromArgb(255, v, t, p);
+                case 1:
+                    return Color.FromArgb(255, q, v, p);
+                case 2:
+                    return Color.FromArgb(255, p, v, t);
+                case 3:
+                    return Color.FromArgb(255, p, q, v);
+                case 4:
+                    return Color.FromArgb(255, t, p, v);
+                default:
+                    return Color.FromArgb(255, v, p, q);
+            }
+        }
+    }
+}
