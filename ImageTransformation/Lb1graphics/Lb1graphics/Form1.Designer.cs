674e7617b3f573e7d78d22040f38ae90a594d8bd
Leskov Andrew <102716018+und3rrr@users.noreply.github.com>
2024-11-26 18:29:13 +0300

    Add files via upload

---
+++ ImageTransformation/Lb1graphics/Lb1graphics/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Lb1graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(527, 408);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(584, 279);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 56);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(558, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(572, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Начальная Х";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Начальная Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(572, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Конечная Х";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(572, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Конечная Y";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(643, 57);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(643, 84);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(643, 110);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 9;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(643, 137);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+    }
+}
+
---
+++ ImageTransformation/Lb1graphics/Lb1graphics/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+namespace Lb1graphics
+{
+    public partial class Form1 : Form
+    {
+        const int pixWidth = 1;//Ширина увеличенного пикселя
+        const int pixHeight = 1;//Высота увеличенного пикселя
+        Point start;
+        Point end;
+        algs cur;
+
+        struct Point
+        {
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public int x, y;
+        }
+
+        enum algs
+        {
+            CDA,
+            IntBres,
+            DoubleBres
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            ListBoxInit();
+            NumInit();
+        }
+
+        void ListBoxInit()
+        {
+            listBox1.Items.Add("алгоритм ЦДА");
+            listBox1.Items.Add("целочисленный алгоритм Брезенхема");
+            listBox1.Items.Add("алгоритм Брезенхема");
+            listBox1.SetSelected(0, true);
+        }
+
+        void NumInit()
+        {
+            numericUpDown1.Maximum = 480 / pixWidth;
+            numericUpDown3.Maximum = 480 / pixWidth;
+            numericUpDown2.Maximum = 480 / pixHeight;
+            numericUpDown4.Maximum = 480 / pixHeight;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start = new Point((int)numericUpDown1.Value, (int)numericUpDown2.Value);
+            end = new Point((int)numericUpDown3.Value, (int)numericUpDown4.Value);
+            cur = toAlgs(listBox1.SelectedIndex);
+            Draw();
+        }
+
+        algs toAlgs(int num)
+        {
+            switch (num)
+            {
+                case 0: return algs.CDA;
+                case 1: return algs.IntBres;
+                default: return algs.DoubleBres;
+            }
+        }
+
+        //Рисование линии
+        void Draw()
+        {
+            Bitmap bmp = new Bitmap(480, 480);
+            switch (cur)
+            {
+                case algs.CDA: CDA(bmp); break;
+                case algs.IntBres: IntBres(bmp); break;
+                case algs.DoubleBres: DoubleBres(bmp); break;
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        void CDA(Bitmap bmp)
+        {
+            if (start.x == end.x && start.y == end.y)
+            {
+                bmp.SetPixel(start.x, start.y, Color.Black);
+                return;
+            }
+            double l;
+            if (Math.Abs(end.x - start.x) > Math.Abs(end.y - start.y))
+                l = Math.Abs(end.x - start.x);
+            else l = Math.Abs(end.y - start.y);
+            double dx = (end.x - start.x) / l, dy = (end.y - start.y) / l;
+            double x = start.x + Sign(dx) * .5f;
+            double y = start.y + Sign(dy) * .5f;
+            for (int i = 1; i <= l + 1; i++)
+            {
+                DrawPixel(bmp, (int)x, (int)y, Color.Black);
+                x += dx;
+                y += dy;
+            }
+        }
+
+        void IntBres(Bitmap bmp)
+        {
+            if (start.x == end.x && start.y == end.y)
+            {
+                bmp.SetPixel(start.x, start.y, Color.Black);
+                return;
+            }
+            double dx = end.x - start.x;
+            double dy = end.y - start.y;
+            int sx = Sign(dx);
+            int sy = Sign(dy);
+            dx = Math.Abs(dx);
+            dy = Math.Abs(dy);
+            bool flag = false;
+            if (dy > dx)
+            {
+                double temp = dx;
+                dx = dy;
+                dy = temp;
+                flag = true;
+            }
+            double f = 2 * dy - dx;
+            double x = start.x, y = start.y;
+            while (x <= end.x)
+            {
+                DrawPixel(bmp, (int)x, (int)y, Color.Black);
+                if (f >= 0)
+                {
+                    if (flag)
+                        x += sx;
+                    else y += sy;
+                    f -= 2 * dx;
+                }
+                else
+                {
+                    if (flag) y += sy;
+                    else x += sx;
+                    f += 2 * dy;
+                }
+            }
+        }
+
+        void DoubleBres(Bitmap bmp)
+        {
+            if (start.x == end.x && start.y == end.y)
+            {
+                bmp.SetPixel(start.x, start.y, Color.Black);
+                return;
+            }
+            double dx = end.x - start.x;
+            double dy = end.y - start.y;
+            int sx = Sign(dx);
+            int sy = Sign(dy);
+            dx = Math.Abs(dx);
+            dy = Math.Abs(dy);
+            bool flag = false;
+            if (dy > dx)
+            {
+                double temp = dx;
+                dx = dy;
+                dy = temp;
+                flag = true;
+            }
+            double f = dy / dx - .5f;
+            double x = start.x, y = start.y;
+            while (x <= end.x)
+            {
+                DrawPixel(bmp, (int)x, (int)y, Color.Black);
+                if (f >= 0)
+                {
+                    if (flag)
+                        x += sx;
+                    else y += sy;
+                    f--;
+                }
+                else
+                {
+                    if (flag) y += sy;
+                    else x += sx;
+                    f += dy / dx;
+                }
+            }
+        }
+
+        int Sign(double n)
+        {
+            return n > 0 ? 1 : n < 0 ? -1 : 0;
+        }
+
+        //Рисование увеличенного пискеля для наглядности
+        void DrawPixel(Bitmap bitmap, int x, int y, Color col)
+        {
+            for (int i = 0; i < pixWidth; i++)
+                for (int j = 0; j < pixHeight; j++)
+                {
+                    bitmap.SetPixel(x + i, y + j, col);
+                }
+        }
+    }
+}
